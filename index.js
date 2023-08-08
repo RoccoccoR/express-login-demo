12cf3972303a6167356a59cb9edc3933cf2d7486
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-08 12:34:23 +0200

    connected

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const app = express();
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  console.log("got a request for /");
+  // res.send("Hi there, everyone!");
+
+  // Find the file index.html in this directory, and send it.
+  const dirName = path.dirname(new URL(import.meta.url).pathname);
+  res.sendFile(path.join(dirName, "/index.html"));
+});
